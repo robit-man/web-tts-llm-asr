93de368314eb12e7dce4a17319ebffbd1ab9dcf9
robit-man <coleman.k.myers@gmail.com>
2025-11-25 14:27:55 -0800

    model fetching

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9vv7u9nrjv8"
+    "revision": "0.2em0go70p68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
